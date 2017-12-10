03052763369f7f521e37a6879063b41fe2824fef
Helene Leblanc <helene.d.m@wanadoo.fr>
2017-12-10 19:39:28 +0100

    Initialisation de la carte sur la page d'accueil

---
+++ vue/carte.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="fr">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Carte</title>
+        <link rel="stylesheet" href="../contenu/css.css">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css"
+   integrity="sha512-M2wvCLH6DSRazYeZRIm1JnYyh22purTM+FDB5CsyxtQJYeKq83arPe5wgbNmcFXGqiSH2XR8dT/fJISVA1r/zQ=="
+   crossorigin=""/>
+        <script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js"
+   integrity="sha512-lInM/apFSqyy1o6s89K4iQUKg6ppXEgsVxT35HbzUupEVRh2Eu9Wdl4tHj7dZO0s1uvplcYGmt3498TtHq+log=="
+   crossorigin=""></script> 
+    </head>
+    
+    <body>
+        <div id="container">
+            <!-- placer le code de ce qui sera au dessus de la carte -->
+        </div>
+        <div id="map"></div>
+        <!--<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCsfoXA-mwUT7PsZADCSNoD6UruZP3gI74&callback=initMap" async defer></script>-->
+        <script type="text/javascript" src="../contenu/js/map.js"></script>
+    </body>
+</html>
